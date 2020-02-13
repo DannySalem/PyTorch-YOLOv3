1942b2293f48b8f4e56a6061f9cbce93d5be1b79
Danny Salem <danny.salem2@gmail.com>
2020-02-13 17:08:29 -0500

    fix Linux pathfiles

--- data/makePathTXT.py
+++ data/makePathTXT.py
@@ -6,20 +6,23 @@ path2 = 'C:/Projects/PyTorch-YOLOv3/data/visdrone/images/val/'
 output1 = '/data/salemd/PyTorch-YOLOv3/data/visdrone/images/train/'
 output2 = '/data/salemd/PyTorch-YOLOv3/data/visdrone/images/val/'
 
-txtfile = open(path1 + 'train.txt', 'a')
+txtpath = 'C:/Projects/PyTorch-YOLOv3/data/visdrone/images/'
+
+txtfile = open(txtpath + 'train.txt', 'a')
 for filename in os.listdir(path1):
     txtfile.write(path1 + filename + '\n')
 
-txtfile = open(path2 + 'val.txt', 'a')
+txtfile = open(txtpath + 'val.txt', 'a')
 for filename in os.listdir(path2):
     txtfile.write(path2 + filename + '\n')
 
 
-txtfile = open(path1 + 'train_Linux.txt', 'a')
+txtfile = open(txtpath + 'train_Linux.txt', 'a')
 for filename in os.listdir(path1):
-    txtfile.write(output1+ filename + '\n')
+    txtfile.write(output1 + filename + '\n')
 
-txtfile = open(path2 + 'val_Linux.txt', 'a')
+txtfile = open(txtpath + 'val_Linux.txt', 'a')
 for filename in os.listdir(path2):
+    print(output2)
     txtfile.write(output2 + filename + '\n')
 
