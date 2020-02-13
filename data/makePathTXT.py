334aefebb21275f02a5571f8d419813cbca09390
Danny Salem <danny.salem2@gmail.com>
2020-02-13 16:36:23 -0500

    add visdrone filepath lists for windows and Linux

--- data/makePathTXT.py
+++ data/makePathTXT.py
@@ -1,14 +1,25 @@
 import os
 
-path1 = 'C:/Users/Salemd/Downloads/'
-path2 = 'VISDrone/Task 1 Object Detection in Images/VisDrone2019-DET-train/images/'
-path3 = 'VISDrone/Task 1 Object Detection in Images/VisDrone2019-DET-val/images/'
+path1 = 'C:/Projects/PyTorch-YOLOv3/data/visdrone/images/train/'
+path2 = 'C:/Projects/PyTorch-YOLOv3/data/visdrone/images/val/'
+
+output1 = '/data/salemd/PyTorch-YOLOv3/data/visdrone/images/train/'
+output2 = '/data/salemd/PyTorch-YOLOv3/data/visdrone/images/val/'
 
 txtfile = open(path1 + 'train.txt', 'a')
-for filename in os.listdir(path1 + path2):
-    txtfile.write(path1 + path2 + filename + '\n')
+for filename in os.listdir(path1):
+    txtfile.write(path1 + filename + '\n')
 
-txtfile = open(path1 + 'val.txt', 'a')
-for filename in os.listdir(path1 + path3):
-    txtfile.write(path1 + path3 + filename + '\n')
+txtfile = open(path2 + 'val.txt', 'a')
+for filename in os.listdir(path2):
+    txtfile.write(path2 + filename + '\n')
+
+
+txtfile = open(path1 + 'train_Linux.txt', 'a')
+for filename in os.listdir(path1):
+    txtfile.write(output1+ filename + '\n')
+
+txtfile = open(path2 + 'val_Linux.txt', 'a')
+for filename in os.listdir(path2):
+    txtfile.write(output2 + filename + '\n')
 
